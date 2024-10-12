c49066cfcb8766b8813cd76e12664bf8c5e71c5d
mspirovv <mspirov@abv.bg>
2024-10-12 20:06:38 +0300

    Added decode to jwt

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -4,10 +4,13 @@ import util from 'util';
 
  const verify = util.promisify(jsonwebtoken.verify);
  const sign = util.promisify(jsonwebtoken.sign);
+ const decode = util.promisify(jsonwebtoken.decode);
 
 const jwt = {
     verify,
-    sign
+    sign,
+    decode,
+    
 }
 
 export default jwt;
